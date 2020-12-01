ac0be501e5bf98c35ec80e789467baaeece26669
Alex Powell <alexjohnpowell93@gmail.com>
2020-12-01 10:33:37 +0000

    Dirty count method

--- 2020/1/accounts.py
+++ 2020/1/accounts.py
@@ -1,13 +1,23 @@
 #!/bin/python
 
 def read_input( filename: str ) -> list:
-    data = []
+    data = [];
     with open( filename, 'r' ) as file:
-        lines = file.readlines()
+        lines = file.readlines();
     for line in lines:
-        data.append( int( line ) )
+        data.append( int( line ) );
     
-    return data
+    return data;
+
+def find_sum( data:list, sum_target:int ) -> int:
+    sum = -1;
+    
+    for item1 in data:
+        for item2 in data:
+            if( item1 + item2 ) == sum_target:
+                return item1 * item2;
+
+    return sum;
 
 val = read_input( "input" );
-print( val )
+print( "Summed value for target is: ", find_sum( val, 2020 ) );
