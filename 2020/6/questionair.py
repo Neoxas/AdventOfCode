c391927c60d6b99f119fa88e6c507be47df8c051
Alex <alexjohnpowell93@gmail.com>
2020-12-06 12:15:12 +0000

    First pass at second star

--- 2020/6/questionair.py
+++ 2020/6/questionair.py
@@ -4,9 +4,17 @@ class Questionair():
         self._raw = data;
         # Dont like work in the constructor. Might remove this
         self.answers = data.replace( '\r', '' ).replace( '\n', '' );
+        self.individual_answers = data.replace( '\r', '' ).split( '\n' );
 
-    def count_answers( self ):
+    def count_answers( self ) -> int:
         return len( set( self.answers ) );
+
+    def count_individual_answers_that_match( self ) -> int:
+        start_set = set( self.individual_answers )
+        for line in self.individual_answers:
+            start_set = start_set & set( self.individual_answers );
+        return len( start_set );
+
 
 def read_questionair( filename: str ) -> list:
     with open( filename, 'r' ) as f:
@@ -17,3 +25,4 @@ def read_questionair( filename: str ) -> list:
 if __name__ == "__main__":
     q = read_questionair( 'input' );
     print( "First Star : {}".format( sum( [ Questionair( c ).count_answers() for c in q ] ) ) ) 
+    print( "Second Star : {}".format( sum( [ Questionair( c ).count_individual_answers_that_match() for c in q ] ) ) ) 
